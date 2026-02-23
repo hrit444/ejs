4dee8bea701a10c06345c82b672f0caa5796f9fd
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2026-02-23 12:03:11 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const morgan = require("morgan")
+
+const app = express()
+
+//using morgan as middleware - logger
+app.use(morgan("dev"))
+
+app.set("view engine", "ejs")
+
+app.post('/api/auth/register', (req,res)=>{
+  res.status(201).json({
+    message: "User registered successfully !"
+  })
+})
+
+app.get('/',(req, res)=>{
+  res.render("index", {message: "hello from ejs"})
+})
+
+module.exports = app
